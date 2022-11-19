a014143cadc873500b73b2537578a285483f4ebc
pwsn21 <pwsn21@gmail.com>
2022-11-18 23:36:02 -0800

    Milestonetracker
    register made no sense.
    app is commented out in constellation settings

---
+++ constellation/milestonetracker/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class user(models.Model):
+    first_name = models.CharField('First Name', max_length=50)
+    last_name = models.CharField('Last Name', max_length=50)
+    employeenumber = models.CharField('Employee ID', max_length=10)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class dops(models.Model):
+    dops_date = models.DateTimeField('DOPS Date')
+    mentor = models.ForeignKey(user, blank=True, null=True, on_delete=models.CASCADE)
+    msnumber = models.IntegerField ('Milestone Number')
+    comments = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.mentor}, {self.dops_date}"
+
+
+
+
+
