008a52395aa2207940a458a50b0705e7cc2a696a
pwsn21 <pwsn21@gmail.com>
2022-11-19 01:08:29 -0800

    DOPS View

--- constellation/milestonetracker/views.py
+++ constellation/milestonetracker/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import dops
 
 def home(request):
     return render(request, 'milestonetracker/home.html', {})
+
+def all_dops(request):
+    all_dops = dops.objects.all()
+
+    return render(request, 'milestonetracker/dops.html', 
+        {'all_dops': all_dops})
